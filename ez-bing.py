576025e3ab86b493f09fdd4499401a650e3f88ca
Isaac Pearlman <isaacbpearlman2010@gmail.com>
2022-08-23 16:53:45 -0500

    Add files via upload

--- ez-bing.py
+++ ez-bing.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import time
 
+a = 1
 
-search = input("What would you like to search you lazy person: ")
-
-driver = webdriver.Chrome()
-
-driver.get("https://bing.com")
-driver.find_element(By.XPATH, '//*[@id="sb_form_q"]').click()
-driver.find_element(By.XPATH, '//*[@id="sb_form_q"]').send_keys(search)
-driver.find_element(By.XPATH, '//*[@id="sb_form_q"]').send_keys(Keys.RETURN)
+while a==1: 
+    search = input("What would you like to search you lazy person: ")
+    time.sleep(0.5)
+    driver = webdriver.Chrome()
+    driver.get("https://bing.com")
+    driver.find_element(By.XPATH, '//*[@id="sb_form_q"]').click()
+    driver.find_element(By.XPATH, '//*[@id="sb_form_q"]').send_keys(search)
+    driver.find_element(By.XPATH, '//*[@id="sb_form_q"]').send_keys(Keys.RETURN)
--- ez-google.py
+++ ez-google.py
@@ -2,12 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time 
-search = input("What would you like to search you lazy person: ")
 
-driver = webdriver.Chrome()
+a = 1
 
-driver.get("https://google.com")
-time.sleep(1)
-sbox = driver.find_element(By.NAME, "q")
-sbox.send_keys(search)
-sbox.send_keys(Keys.RETURN)
+while a==1:
+    search = input("What would you like to search you lazy person: ")
+    time.sleep(0.5)
+    driver = webdriver.Chrome()
+
+    driver.get("https://google.com")
+    sbox = driver.find_element(By.NAME, "q")
+    sbox.send_keys(search)
+    sbox.send_keys(Keys.RETURN)
